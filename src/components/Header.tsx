32f3a32c87510b1a82e2e2a022a33d8d7329f03d
charlielovett <115426211+charlielovett@users.noreply.github.com>
2025-03-05 17:57:06 -0600

    style(Header): font and react-icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,10 @@ import { IoSettingsOutline } from "react-icons/io5";
 export default function Header() {
     return (
         <header className="flex flex-row justify-between items-center h-16 mx-8 mt-8">
-            <h2 className="text-2xl">Daily Rank</h2>
-            <IoSettingsOutline size={32} />
+            <h2 className="text-4xl font-bold">Daily Rank</h2>
+            <div className="bg-[#EEE] rounded-full p-3">
+                <IoSettingsOutline size={32} color="#7E71FC" />
+            </div>
         </header>
     )
 };

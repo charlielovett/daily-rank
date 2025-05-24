5bf3a22aa7825d102515e83c6d6aaece83754538
charlielovett <115426211+charlielovett@users.noreply.github.com>
2025-05-24 17:38:26 -0500

    feat: redux provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,15 +1,18 @@
-import '../styles/globals.css';
+import '../app/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '@/components/Header';
+import Providers from '@/app/providers';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
+      <Providers>
         <div className="flex flex-col h-screen overflow-hidden">
-            <Header /> {/* Adjust height and color as needed */}
-            <main className="flex-1 overflow-y-auto">
-                <Component {...pageProps} />
-            </main>
+          <Header /> {/* Adjust height and color as needed */}
+          <main className="flex-1 overflow-y-auto">
+            <Component {...pageProps} />
+          </main>
         </div>
+      </Providers>
     );
 }
 
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -15,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 } satisfies Config;
